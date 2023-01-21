d227edcaa6504778f040676e6c2e46906825c32f
JasonShuyinta <jasonshuyinta@gmail.com>
2023-01-21 12:26:20 +0100

    rabbitMq message broker started

--- customer/src/main/java/com/dotjson/customer/CustomerService.java
+++ customer/src/main/java/com/dotjson/customer/CustomerService.java
@@ -1,15 +1,18 @@
 package com.dotjson.customer;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+    //private final RestTemplate restTemplate;
+    private RabbitTemplate rabbitTemplate;
 
 
     public void registerCustomer(CustomerRegistrationRequest request) {
@@ -19,14 +22,17 @@ public class CustomerService {
                 .email(request.email())
                 .build();
         customerRepository.saveAndFlush(customer);
-         FraudCheckResponse response = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-                );
-         if(response.isFraudster()) {
-             throw new IllegalStateException("fraudster");
-         }
+        rabbitTemplate.convertAndSend(RabbitMQConfig.MESSAGE_EXCHANGE, RabbitMQConfig.ROUTING_KEY, customer.getId());
+        log.info("Message published");
+
+        //         FraudCheckResponse response = restTemplate.getForObject(
+//                "http://FRAUD/api/v1/fraud-check/{customerId}",
+//                FraudCheckResponse.class,
+//                customer.getId()
+//                );
+//         if(response.isFraudster()) {
+//             throw new IllegalStateException("fraudster");
+//         }
 
     }
 }
